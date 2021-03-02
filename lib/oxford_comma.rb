9e4e0aee38843e82d3a061a9ebd1b0b0ae30e882
rsmith <legendary.belief@gmail.com>
2021-03-02 13:30:12 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length > 2
+        other = array.pop
+        strng = array.join(", ")
+        strng << ", and #{other}"
+    else
+        array.join(" and ")
+    end
 end
+
+#puts oxford_comma(["sam", "josh", "rob", "steve"])
